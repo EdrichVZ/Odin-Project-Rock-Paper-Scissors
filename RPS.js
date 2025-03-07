e3b6b350a4c4fa27c58553aaff9e42c7e5a207ca
EdrichVZ <171524023+EdrichVZ@users.noreply.github.com>
2025-03-07 07:02:34 +0200

    Added user.human input function

--- RPS.js
+++ RPS.js
@@ -5,15 +5,24 @@ function getComputerChoice() {
     let computer_choice = Math.round(Math.random() * (3 - 1) + 1);
 
     if (computer_choice === 1){
-        computer_choice = "Rock";
+        computer_choice = "rock";
         return computer_choice;
     } else if (computer_choice === 2){
-        computer_choice = "Paper";
+        computer_choice = "paper";
         return computer_choice;
     } else {
-        computer_choice = "Scissors";
+        computer_choice = "scissors";
         return computer_choice;
     }
 }
 
+console.log(getComputerChoice())
 
+// Function for player/user choice:
+function getHumanChoice() {
+    let human_choice = prompt("Pick Rock, Paper or Scissors")
+    human_choice = human_choice.toLowerCase();
+    return human_choice
+}
+
+console.log(getHumanChoice())
