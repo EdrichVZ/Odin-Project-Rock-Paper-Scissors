15773e70d8345fb20af1e81e48739c14ecd0daf8
EdrichVZ <171524023+EdrichVZ@users.noreply.github.com>
2025-03-07 06:53:49 +0200

    Function and logic for computer choice added

--- RPS.js
+++ RPS.js
@@ -1,1 +1,19 @@
 console.log("Welcome to Rock Paper Scissors!")
+
+// Creating function for random computer choice with Math.random and if statements.
+function getComputerChoice() {
+    let computer_choice = Math.round(Math.random() * (3 - 1) + 1);
+
+    if (computer_choice === 1){
+        computer_choice = "Rock";
+        return computer_choice;
+    } else if (computer_choice === 2){
+        computer_choice = "Paper";
+        return computer_choice;
+    } else {
+        computer_choice = "Scissors";
+        return computer_choice;
+    }
+}
+
+
