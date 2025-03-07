763f5cda6974ae77b9950a0870031e6d5888b28b
EdrichVZ <171524023+EdrichVZ@users.noreply.github.com>
2025-03-07 07:41:13 +0200

    Added 5 round play function

--- RPS.js
+++ RPS.js
@@ -33,25 +33,25 @@ function playRound(humanChoice, computerChoice) {
         console.log("Rock vs Rock -> DRAW!");
     } else if (humanChoice === "paper" && computerChoice === "rock"){
         console.log("Paper vs Rock -> YOU WIN!");
-        return "H"
+        return "H";
     } else if (humanChoice === "scissors" && computerChoice === "rock"){
         console.log("Scissors vs Rock -> COMPUTER WINS!");
-        return "C"
+        return "C";
     } else if (humanChoice === "rock" && computerChoice === "paper"){
         console.log("Rock vs Paper -> COMPUTER WINS!");
-        return "C"
+        return "C";
     } else if (humanChoice === "paper" && computerChoice === "paper"){
         console.log("Paper vs Paper -> DRAW!");
     } else if (humanChoice === "scissors" && computerChoice === "paper"){
         console.log("Scissors vs Paper -> YOU WIN!");
-        return "H"
+        return "H";
     } else if (humanChoice === "rock" && computerChoice === "scissors"){
         console.log("Rock vs Scissors -> YOU WIN!");
-        return "H"
+        return "H";
     } else if (humanChoice === "paper" && computerChoice === "scissors"){
         console.log("Paper vs Scissors -> COMPUTER WINS!");
-        return "C"
-    }else if (humanChoice === "scissors" && computerChoice === "scissors"){
+        return "C";
+    } else if (humanChoice === "scissors" && computerChoice === "scissors"){
         console.log("Scissors vs Scissors -> DRAW!");
     }
 }
@@ -59,3 +59,31 @@ function playRound(humanChoice, computerChoice) {
 //playRound(getHumanChoice(), getComputerChoice())
 
 // Function for 5 rounds of play with score tracking:
+function playGame(){
+    let humanscore = 0
+    let computerscore = 0
+    for (let r = 0; r < 5; r++) {
+        winner = playRound(getHumanChoice(), getComputerChoice())
+        if (winner === "H") {
+            humanscore += 1;
+        } else if (winner === "C") {
+            computerscore += 1;
+        } else {
+            console.log("Draw");
+        }
+    }
+
+    if (computerscore > humanscore) {
+        console.log("Computer has "+computerscore+"");
+        console.log("You have "+humanscore+"");
+        console.log("The Computer wins!");        
+    } else if(computerscore < humanscore) {
+        console.log("Computer has "+computerscore+"");
+        console.log("You have "+humanscore+"");
+        console.log("You win!"); 
+    } else {
+        console.log("Computer has "+computerscore+"");
+        console.log("You have "+humanscore+"");
+        console.log("It's a Draw!"); 
+    }
+}
