21050c87d047cd4ad58466aebc40ec293bfef973
EdrichVZ <171524023+EdrichVZ@users.noreply.github.com>
2025-03-13 20:26:45 +0200

    Created variables

--- RPS.js
+++ RPS.js
@@ -3,4 +3,13 @@ const game = () => {
     let playerScore = 0;
     let compterScore = 0;
     
+    //Function to play the game:
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorsBtn = document.querySelector('.scissors');
+        // Add buttons and computer moves to seperate arrays:
+        const playerOptions = [rockBtn, paperBtn, scissorsBtn];
+        const computerOptions = ['rock', 'paper', 'scissors'];
+    }
 }
